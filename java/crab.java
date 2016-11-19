3f30fcb0e820fbfac8031f00b6169747fa38b47f
hurjun <hj2002ca@hanmail.net>
2016-11-19 23:01:45 +0900

    Revert "Add files via upload"

--- java/crab.java
+++ java/crab.java
@@ -10,7 +10,6 @@ public class crab extends Animal
 {
     int i = 0;
     int score;
-    int temp = 0;
     /**
      * Act - do whatever the crab wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -36,13 +35,12 @@ public class crab extends Animal
         }
         }
         else if (score == 200){
-            for(; i < 1;i++){
+        	for(; i < 1;i++){
             getWorld().addObject(new lobster(), 100, 200);
-            
         }
         }
         else if (score == 300){
-            for(; i < 1;i++){
+        	for(; i < 1;i++){
             getWorld().addObject(new lobster(), 100, 200);
         }
         }
@@ -51,18 +49,9 @@ public class crab extends Animal
     public void lookForWorm(){
         if ( isTouching(worm.class) ){
            World w = getWorld();
-            Greenfoot gf = new Greenfoot();
            removeTouching(worm.class);
-          score = score + 10;
+          
            w.showText("score : "+ score , 60, 30);
-         
-            
-           if(score >= 1000){
-                w.showText("성공", 300,250);
-                gf.stop();
-                w.stopped();
-           }
         }
-           
      }
 }
